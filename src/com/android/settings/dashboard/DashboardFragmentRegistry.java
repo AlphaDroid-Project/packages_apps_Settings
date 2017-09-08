09c60ffc587a7fa7a2d0aa2ef2f26ffb4edd05d2
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate AlphaSettings into Settings
    Change-Id: Id86228181a2ae84008a177d42d1fc94317eb2dc8
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/dashboard/DashboardFragmentRegistry.java
+++ src/com/android/settings/dashboard/DashboardFragmentRegistry.java
@@ -50,6 +50,8 @@ import com.android.settings.security.SecuritySettings;
 import com.android.settings.system.SystemDashboardFragment;
 import com.android.settingslib.drawer.CategoryKey;
 
+import com.alpha.settings.AlphaSettings;
+
 import java.util.Map;
 
 /**
@@ -132,6 +134,8 @@ public class DashboardFragmentRegistry {
                 CategoryKey.CATEGORY_SPECIAL_APP_ACCESS);
         PARENT_TO_CATEGORY_KEY_MAP.put(MoreSecurityPrivacyFragment.class.getName(),
                 CategoryKey.CATEGORY_MORE_SECURITY_PRIVACY_SETTINGS);
+        PARENT_TO_CATEGORY_KEY_MAP.put(AlphaSettings.class.getName(),
+                CategoryKey.CATEGORY_ALPHA);
 
         CATEGORY_KEY_TO_PARENT_MAP = new ArrayMap<>(PARENT_TO_CATEGORY_KEY_MAP.size());
 
