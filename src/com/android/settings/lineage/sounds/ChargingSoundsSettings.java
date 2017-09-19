LineageOS/android_packages_apps_LineageParts

LineageParts: rebrand step 2: update file contents
Change-Id: I8b5f424806d9506f2eac4a034e792ae6e3d79455

src/org/lineageos/lineageparts/sounds/ChargingSoundsSettings.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.cyanogenmod.cmparts.sounds;
+package org.lineageos.lineageparts.sounds;
 
 import android.app.Activity;
 import android.content.Context;
@@ -28,10 +28,10 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.support.v7.preference.Preference;
 
-import cyanogenmod.providers.CMSettings;
+import lineageos.providers.LineageSettings;
 
-import org.cyanogenmod.cmparts.R;
-import org.cyanogenmod.cmparts.SettingsPreferenceFragment;
+import org.lineageos.lineageparts.R;
+import org.lineageos.lineageparts.SettingsPreferenceFragment;
 
 public class ChargingSoundsSettings extends SettingsPreferenceFragment {
 
@@ -58,8 +58,8 @@ public class ChargingSoundsSettings extends SettingsPreferenceFragment {
         }
 
         mChargingSoundsRingtone = findPreference(KEY_CHARGING_SOUNDS_RINGTONE);
-        String curTone = CMSettings.Global.getString(getContentResolver(),
-                CMSettings.Global.POWER_NOTIFICATIONS_RINGTONE);
+        String curTone = LineageSettings.Global.getString(getContentResolver(),
+                LineageSettings.Global.POWER_NOTIFICATIONS_RINGTONE);
         if (curTone == null) {
             updateChargingRingtone(Settings.System.DEFAULT_NOTIFICATION_URI.toString(), true);
         } else {
@@ -90,8 +90,8 @@ public class ChargingSoundsSettings extends SettingsPreferenceFragment {
 
         mChargingSoundsRingtone.setSummary(toneName);
         if (persist) {
-            CMSettings.Global.putString(getContentResolver(),
-                    CMSettings.Global.POWER_NOTIFICATIONS_RINGTONE, toneUriString);
+            LineageSettings.Global.putString(getContentResolver(),
+                    LineageSettings.Global.POWER_NOTIFICATIONS_RINGTONE, toneUriString);
         }
     }
 
@@ -99,8 +99,8 @@ public class ChargingSoundsSettings extends SettingsPreferenceFragment {
     public boolean onPreferenceTreeClick(Preference preference) {
         if (preference == mChargingSoundsRingtone) {
             launchNotificationSoundPicker(REQUEST_CODE_CHARGING_NOTIFICATIONS_RINGTONE,
-                    CMSettings.Global.getString(getContentResolver(),
-                    CMSettings.Global.POWER_NOTIFICATIONS_RINGTONE));
+                    LineageSettings.Global.getString(getContentResolver(),
+                    LineageSettings.Global.POWER_NOTIFICATIONS_RINGTONE));
         }
         return super.onPreferenceTreeClick(preference);
     }

src/org/lineageos/lineageparts/utils/ResourceUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.cyanogenmod.cmparts.utils;
+package org.lineageos.lineageparts.utils;
 
 import android.content.res.Resources;
 import android.util.Log;
@@ -33,7 +33,7 @@ public class ResourceUtils {
     public static String getStringForResourceName(final Resources res,
                                                   final String resourceName,
                                                   final String defaultValue) {
-        final int resId = res.getIdentifier(resourceName, "string", "org.cyanogenmod.cmparts");
+        final int resId = res.getIdentifier(resourceName, "string", "org.lineageos.lineageparts");
         if (resId <= 0) {
             Log.e(TAG, "No resource found for " + resourceName);
             return defaultValue;

AlphaDroid-Project/packages_apps_Settings

src/com/android/settings/lineage/sounds/ChargingSoundsSettings.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.cyanogenmod.cmparts.sounds;
+package com.android.settings.lineage.sounds;
 
 import android.app.Activity;
 import android.content.Context;
@@ -28,10 +28,10 @@ import android.os.Vibrator;
 import android.provider.Settings;
 import android.support.v7.preference.Preference;
 
-import cyanogenmod.providers.CMSettings;
+import android.provider.Settings;
 
-import org.cyanogenmod.cmparts.R;
-import org.cyanogenmod.cmparts.SettingsPreferenceFragment;
+import com.android.settings.R;
+import com.android.settings.lineage.SettingsPreferenceFragment;
 
 public class ChargingSoundsSettings extends SettingsPreferenceFragment {
 
@@ -58,8 +58,8 @@ public class ChargingSoundsSettings extends SettingsPreferenceFragment {
         }
 
         mChargingSoundsRingtone = findPreference(KEY_CHARGING_SOUNDS_RINGTONE);
-        String curTone = CMSettings.Global.getString(getContentResolver(),
-                CMSettings.Global.POWER_NOTIFICATIONS_RINGTONE);
+        String curTone = Settings.Global.getString(getContentResolver(),
+                Settings.Global.POWER_NOTIFICATIONS_RINGTONE);
         if (curTone == null) {
             updateChargingRingtone(Settings.System.DEFAULT_NOTIFICATION_URI.toString(), true);
         } else {
@@ -90,8 +90,8 @@ public class ChargingSoundsSettings extends SettingsPreferenceFragment {
 
         mChargingSoundsRingtone.setSummary(toneName);
         if (persist) {
-            CMSettings.Global.putString(getContentResolver(),
-                    CMSettings.Global.POWER_NOTIFICATIONS_RINGTONE, toneUriString);
+            Settings.Global.putString(getContentResolver(),
+                    Settings.Global.POWER_NOTIFICATIONS_RINGTONE, toneUriString);
         }
     }
 
@@ -99,8 +99,8 @@ public class ChargingSoundsSettings extends SettingsPreferenceFragment {
     public boolean onPreferenceTreeClick(Preference preference) {
         if (preference == mChargingSoundsRingtone) {
             launchNotificationSoundPicker(REQUEST_CODE_CHARGING_NOTIFICATIONS_RINGTONE,
-                    CMSettings.Global.getString(getContentResolver(),
-                    CMSettings.Global.POWER_NOTIFICATIONS_RINGTONE));
+                    Settings.Global.getString(getContentResolver(),
+                    Settings.Global.POWER_NOTIFICATIONS_RINGTONE));
         }
         return super.onPreferenceTreeClick(preference);
     }

src/com/android/settings/lineage/utils/ResourceUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.cyanogenmod.cmparts.utils;
+package com.android.settings.lineage.utils;
 
 import android.content.res.Resources;
 import android.util.Log;
@@ -33,7 +33,7 @@ public class ResourceUtils {
     public static String getStringForResourceName(final Resources res,
                                                   final String resourceName,
                                                   final String defaultValue) {
-        final int resId = res.getIdentifier(resourceName, "string", "org.cyanogenmod.cmparts");
+        final int resId = res.getIdentifier(resourceName, "string", "com.android.settings");
         if (resId <= 0) {
             Log.e(TAG, "No resource found for " + resourceName);
             return defaultValue;