8fb799ab362f990693e0be371c768dacf898fb0e
Daniel Micay <danielmicay@gmail.com>
2019-03-17 18:55:25 -0400

    do not allow disabling the default keyboard
    It's not obvious that disabling it after installing another keyboard is
    a very bad idea. Other keyboards rarely support Direct Boot and won't
    work for entering the password, forcing recovery by plugging in a
    physical keyboard. Even if they do support Direct Boot it's a bad idea
    to rely on third party apps for basic functionality to keep working.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/applications/appinfo/AppButtonsPreferenceController.java
+++ src/com/android/settings/applications/appinfo/AppButtonsPreferenceController.java
@@ -588,6 +588,7 @@ public class AppButtonsPreferenceController extends BasePreferenceController imp
         // by not allowing disabling of apps signed with the
         // system cert and any launcher app in the system.
         if (mHomePackages.contains(mAppEntry.info.packageName)
+                || mAppEntry.info.packageName.equals("com.android.inputmethod.latin")
                 || mAppEntry.info.packageName.equals(GmsCompatApp.PKG_NAME)
                 || mAppEntry.info.packageName.equals(GmsCompatApp.PKG_NAME + ".config")
                 || isSystemPackage(mActivity.getResources(), mPm, mPackageInfo)) {
