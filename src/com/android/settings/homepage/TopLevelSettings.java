abf153660bfd25832728083185054afab20bf942
elpaablo <donopwhile1@gmail.com>
2022-09-20 20:25:40 +0100

    Introduce Nusantara dashboard style
    Adapted from commits:
    * https://github.com/NusantaraProject-ROM/android_packages_apps_Settings/commit/b3fd5d607146d6e88d95e8e61e26fc6df78c57e5
    * https://github.com/NusantaraProject-ROM/android_packages_apps_Settings/commit/c8ebad09bc1272403c5af806e18f2ef96394299e
    * https://github.com/NusantaraProject-ROM/android_packages_apps_Settings/commit/024d5d65051b1900b9ca3b9aa02ee371ca922fc6
    
    So, all credits to Nusantara team
    
    Change-Id: I6d6b5cbeeec95ab8ca776bf1b6471564b9d3c8a8

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -83,8 +83,10 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 return R.xml.top_level_settings;
             case 1:
                 return R.xml.top_level_settings_dot;
+            case 2:
+                return R.xml.top_level_settings_nad;
             default:
-                return R.xml.top_level_settings_dot;
+                return R.xml.top_level_settings_nad;
         }
     }
 
@@ -276,6 +278,8 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 } else if (key.equals("top_level_wellbeing")) {
                     preference.setLayoutResource(R.layout.dot_dashboard_preference_middle);
                 }
+            } else if (mDashBoardStyle == 2) {
+                preference.setLayoutResource(R.layout.nad_dashboard_preference);
             }
         }
     }
@@ -454,7 +458,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
     private void setDashboardStyle(Context context) {
         mDashBoardStyle = Settings.System.getIntForUser(context.getContentResolver(),
-                Settings.System.SETTINGS_DASHBOARD_STYLE, 1, UserHandle.USER_CURRENT);
+                Settings.System.SETTINGS_DASHBOARD_STYLE, 2, UserHandle.USER_CURRENT);
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
