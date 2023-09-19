698b2a1f1dc411c0b245314a040795abe8c1c92d
elpaablo <donopwhile1@gmail.com>
2023-09-19 07:18:20 +0200

    FastChargingPreferenceController: implement dynamic summary to show fastcharge status

--- src/com/android/settings/fuelgauge/FastChargingPreferenceController.java
+++ src/com/android/settings/fuelgauge/FastChargingPreferenceController.java
@@ -21,32 +21,98 @@ import android.os.RemoteException;
 import android.util.Log;
 
 import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
+import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 
-import vendor.lineage.fastcharge.V1_0.IFastCharge;
+import com.android.settingslib.core.lifecycle.events.OnStart;
+import com.android.settingslib.core.lifecycle.events.OnResume;
 
 import java.util.NoSuchElementException;
 
-public class FastChargingPreferenceController extends BasePreferenceController {
+import vendor.lineage.fastcharge.V1_0.IFastCharge;
+import vendor.lineage.fastcharge.V1_0.IRestrictedCurrent;
+
+public class FastChargingPreferenceController extends BasePreferenceController
+        implements OnStart, OnResume {
 
     private static final String KEY_FAST_CHARGING = "fast_charging";
     private static final String TAG = "FastChargingPreferenceController";
 
     private final boolean DEBUG = false;
 
+    private IFastCharge mFastChargeService = null;
+    private IRestrictedCurrent mRestrictedCurrentService = null;
+
+    private Preference mFastChargePref;
+    private boolean mEnabled = true;
 
     public FastChargingPreferenceController(Context context) {
         super(context, KEY_FAST_CHARGING);
+        String iStr = "IFastCharge";
+        try {
+            mFastChargeService = IFastCharge.getService();
+            iStr = "IRestrictedCurrent";
+            mRestrictedCurrentService = IRestrictedCurrent.getService();
+        } catch (NoSuchElementException | RemoteException e) {
+            if (DEBUG) Log.e(TAG, "Failed to get " + iStr + " interface", e);
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        mFastChargePref = screen.findPreference(KEY_FAST_CHARGING);
+    }
+
+    @Override
+    public void onStart() {
+        updateSummary();
+    }
+
+    @Override
+    public void onResume() {
+        updateSummary();
     }
 
     @Override
     public int getAvailabilityStatus() {
+        return mFastChargeService != null ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        boolean enabled;
+        int current;
         try {
-            return IFastCharge.getService() != null ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
-        } catch (NoSuchElementException | RemoteException e) {
-            if (DEBUG) Log.e(TAG, "Failed to get IFastCharge interface", e);
+            enabled = mFastChargeService.isEnabled();
+        } catch (RemoteException e) {
+            if (DEBUG) Log.e(TAG, "isEnabled failed", e);
+            return "";
         }
-        return UNSUPPORTED_ON_DEVICE;
+        if (enabled) {
+            return mContext.getString(R.string.string_enabled);
+        }
+
+        if (mRestrictedCurrentService == null) {
+            return mContext.getString(R.string.string_disabled);
+        }
+
+        try {
+            current = mRestrictedCurrentService.getRestrictedCurrent();
+        } catch (RemoteException e) {
+            if (DEBUG) Log.e(TAG, "getRestrictedCurrent failed", e);
+            return mContext.getString(R.string.string_disabled);
+        }
+
+        return mContext.getString(R.string.fastcharge_disabled_extended_summary,
+                String.valueOf(current) + " mA");
+    }
+
+    private void updateSummary() {
+        if (mFastChargeService != null && mFastChargePref != null) {
+            mFastChargePref.setSummary(getSummary());
+        }
     }
 }
