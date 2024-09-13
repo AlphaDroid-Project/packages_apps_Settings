fc9e4d7059bc4cef2c364f9b58752f8e46167590
kamikaonashi <thorschi93@gmx.at>
2024-09-13 21:20:03 +0200

    Add BatteryDivider to fuelgauge
    some devices with prebuilt kernel report, example 4mAh, instead of 4000mAh or whatever current mAh is.
    add config to let override the divider
    
    device tree example:
    kamikaonashi/device_xiaomi_stone@830a87b

--- src/com/android/settings/fuelgauge/BatteryHeaderPreferenceController.java
+++ src/com/android/settings/fuelgauge/BatteryHeaderPreferenceController.java
@@ -47,6 +47,7 @@ public class BatteryHeaderPreferenceController extends BasePreferenceController
     private final BatterySettingsFeatureProvider mBatterySettingsFeatureProvider;
 
     private BatteryTip mBatteryTip;
+    private int mBatteryDivider;
 
     public BatteryHeaderPreferenceController(Context context, String key) {
         super(context, key);
@@ -55,6 +56,7 @@ public class BatteryHeaderPreferenceController extends BasePreferenceController
                 FeatureFactory.getFeatureFactory().getBatteryStatusFeatureProvider();
         mBatterySettingsFeatureProvider =
                 FeatureFactory.getFeatureFactory().getBatterySettingsFeatureProvider();
+        mBatteryDivider = context.getResources().getInteger(R.integer.config_battery_divider);
     }
 
     @Override
@@ -167,7 +169,7 @@ public class BatteryHeaderPreferenceController extends BasePreferenceController
         mBatteryUsageProgressBarPref.setPercent(batteryLevel, BATTERY_MAX_LEVEL);
 
         if (chargeCounterUah > 0) {
-            int chargeCounter = chargeCounterUah / 1_000;
+            int chargeCounter = chargeCounterUah / mBatteryDivider;
             mBatteryUsageProgressBarPref.setTotalSummary(
                     formatBatteryChargeCounterText(chargeCounter));
         }
