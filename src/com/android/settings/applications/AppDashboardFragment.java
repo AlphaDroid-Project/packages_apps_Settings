c1adf7f509b7394bef1b0ccb9c8cc385ad08a62e
minaripenguin <minaripenguin@users.noreply.github.com>
2024-11-22 05:51:47 +0100

    settings: Introduce QuickSwitch feature [2/2]
    Co-authored-by: elpaablo <donopwhile1@gmail.com>

--- src/com/android/settings/applications/AppDashboardFragment.java
+++ src/com/android/settings/applications/AppDashboardFragment.java
@@ -18,10 +18,14 @@ package com.android.settings.applications;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.os.Bundle;
+import android.os.SystemProperties;
 import android.provider.SearchIndexableResource;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.settings.R;
@@ -36,15 +40,60 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.android.internal.util.alpha.SystemRestartUtils;
+
 /** Settings page for apps. */
 // LINT.IfChange
 @SearchIndexable
-public class AppDashboardFragment extends DashboardFragment {
+public class AppDashboardFragment extends DashboardFragment
+        implements Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "AppDashboardFragment";
     private static final String ADVANCED_CATEGORY_KEY = "advanced_category";
     private static final String ASPECT_RATIO_PREF_KEY = "aspect_ratio_apps";
     private AppsPreferenceController mAppsPreferenceController;
+
+    private static final String QUICK_SWITCH_KEY = "persist.sys.default_launcher";
+
+    private ListPreference quickSwitchListPref;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        int defaultLauncher = SystemProperties.getInt(QUICK_SWITCH_KEY, 0);
+        quickSwitchListPref = findPreference(QUICK_SWITCH_KEY);
+        if (quickSwitchListPref != null) {
+            quickSwitchListPref.setOnPreferenceChangeListener(this);
+            List<String> launcherEntries = new ArrayList<>();
+            List<String> launcherValues = new ArrayList<>();
+            // 0-Launcher3, 1-NexusLauncher, 2-Lawnchair
+            launcherEntries.add(getContext().getResources().getString(R.string.quickswitch_launcher3));
+            launcherValues.add("0");
+            if (AppUtils.isNexusLauncherAvailable()) {
+                launcherEntries.add(getContext().getResources().getString(R.string.quickswitch_nexuslauncher));
+                launcherValues.add("1");
+            }
+            if (AppUtils.isLawnchairAvailable()) {
+                launcherEntries.add(getContext().getResources().getString(R.string.quickswitch_lawnchair));
+                launcherValues.add("2");
+            }
+            quickSwitchListPref.setEntries(launcherEntries.toArray(new CharSequence[launcherEntries.size()]));
+            quickSwitchListPref.setEntryValues(launcherValues.toArray(new CharSequence[launcherValues.size()]));
+            if (defaultLauncher >= 0 && defaultLauncher < launcherValues.size()) {
+                quickSwitchListPref.setValue(String.valueOf(defaultLauncher));
+            }
+        }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == quickSwitchListPref) {
+            SystemRestartUtils.showSystemRestartDialog(getContext());
+            return true;
+        }
+        return false;
+    }
 
     private static List<AbstractPreferenceController> buildPreferenceControllers(Context context) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
