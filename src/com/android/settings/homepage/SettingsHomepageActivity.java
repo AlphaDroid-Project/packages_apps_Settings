fb5d64af3570ce55204ad3f1068bbc208952d7a4
elpaablo <donopwhile1@gmail.com>
2025-10-07 00:40:17 +0100

    Don't show homepage suggestions

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -268,7 +268,7 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         if (!getSystemService(ActivityManager.class).isLowRamDevice()) {
             final boolean scrollNeeded = mIsEmbeddingActivityEnabled
                     && !TextUtils.equals(getString(DEFAULT_HIGHLIGHT_MENU_KEY), highlightMenuKey);
-            showSuggestionFragment(scrollNeeded);
+            // showSuggestionFragment(scrollNeeded);
             if (!Flags.updatedSuggestionCardAosp()
                     && FeatureFlagUtils.isEnabled(this, FeatureFlags.CONTEXTUAL_HOME)) {
                 showFragment(() -> new ContextualCardsFragment(), R.id.contextual_cards_content);
